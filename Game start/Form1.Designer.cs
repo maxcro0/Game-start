cd31af052d3e6f7bbb30235ee63e43f44bed1899
Maxwell Croft <maxwcrof648@ed.amdsb.ca>
2023-09-12 10:52:32 -0400

    Add project files.

---
+++ Game start/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Game_start
+{
+    partial class gameBackground
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gameBackground));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.gameStartText = new System.Windows.Forms.Label();
+            this.Countdown = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonStart.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonStart.Location = new System.Drawing.Point(157, 202);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(117, 40);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start game?";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gameStartText
+            // 
+            this.gameStartText.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameStartText.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.gameStartText.Location = new System.Drawing.Point(46, 39);
+            this.gameStartText.Name = "gameStartText";
+            this.gameStartText.Size = new System.Drawing.Size(342, 57);
+            this.gameStartText.TabIndex = 1;
+            this.gameStartText.Text = "Game starting in:";
+            this.gameStartText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.gameStartText.Click += new System.EventHandler(this.gameStartText_Click);
+            // 
+            // Countdown
+            // 
+            this.Countdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Countdown.ForeColor = System.Drawing.Color.Transparent;
+            this.Countdown.Location = new System.Drawing.Point(154, 96);
+            this.Countdown.Name = "Countdown";
+            this.Countdown.Size = new System.Drawing.Size(120, 102);
+            this.Countdown.TabIndex = 2;
+            this.Countdown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Countdown.Visible = false;
+            this.Countdown.Click += new System.EventHandler(this.Countdown_Click);
+            // 
+            // gameBackground
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(430, 420);
+            this.Controls.Add(this.Countdown);
+            this.Controls.Add(this.gameStartText);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "gameBackground";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label gameStartText;
+        private System.Windows.Forms.Label Countdown;
+    }
+}
+
---
+++ Game start/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Game_start
+{
+    public partial class gameBackground : Form
+    {
+        public gameBackground()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   SoundPlayer beepPlayer = new SoundPlayer(Properties.Resources.gameBeep);
+            SoundPlayer revPlayer = new SoundPlayer(Properties.Resources._461012__matrixxx__retro_wormhole_01);
+            SoundPlayer startPlayer = new SoundPlayer(Properties.Resources._403007__inspectorj__ui_confirmation_alert_a2);
+           
+            buttonStart.Visible = false;
+            gameStartText.Visible = true;
+            gameStartText.Text = "Game Starting in:";
+            beepPlayer.Play();
+            Update();  
+            Countdown.Text = "3";
+            Countdown.Visible = true;
+            Update();
+            Thread.Sleep(1000);
+            beepPlayer.Play();
+            Countdown.Text = "2";
+            Update();
+            Thread.Sleep(1000);
+            beepPlayer.Play();
+            Countdown.Text = "1";
+            Update();
+            Thread.Sleep(1000);
+            Update();   
+            startPlayer.Play();
+            Countdown.Text = "GO!!!!";
+            gameStartText.Visible=false;
+            BackColor = Color.Green;
+          
+
+
+
+            
+
+
+
+        }
+
+        private void gameStartText_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Countdown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
